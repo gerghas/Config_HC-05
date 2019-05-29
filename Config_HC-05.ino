66eee64127fc6529bf46e5dfa989c0ef1261db50
Gerardo Díaz <gerghas@users.noreply.github.com>
2019-05-29 16:00:45 -0500

    Mejora de instrucciones en monitor serial

--- Config_HC-05.ino
+++ Config_HC-05.ino
@@ -1,8 +1,12 @@
-// Monitor serial en NL & CR a 9600
-// Enviar la palabra AT para comprobar que la cominicación sea correcta
-// Para cambiar el nombre al dispositivo usar el comando: AT+NAME="Nombre"
-// Para consultar la contraseña usar el comando: AT+PSWD?
-// Cambiar la contraseña usar el comando: AT+PSWD="Nueva contraseña de 4 caracteres"
+// Para poder configurar el HC-05 mediante el monitor serial, requerimos cargar este código
+// en nuestro Arduino y despues seguir los siguientes pasos:
+// *En caso de tener error durante la carga, desconecta el HC-05 y al terminar la carga vuelve a conectar*
+//
+// 1.- Poner nuestro monitor serial en NL & CR a 9600
+// 2.- Enviar la palabra AT para comprobar que la cominicación sea correcta, reciniremos un "OK"
+// 3.- Para cambiar el nombre al dispositivo usar el comando: AT+NAME="Nombre"
+// 4.- Para consultar la contraseña usar el comando: AT+PSWD?
+// 5.- Cambiar la contraseña usar el comando: AT+PSWD="Nueva contraseña de 4 caracteres"
 
 #include <SoftwareSerial.h> //Se Inclute la librería.
 SoftwareSerial BT(11,10); // Aquí se definen los pines para RX y TX del módulo al Arduino.
